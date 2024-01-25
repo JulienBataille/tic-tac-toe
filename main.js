1b310800f085d4f1c8cac7c01d1a03303e5d628e
landesprit <julien.bataille40@gmail.com>
2024-01-25 14:13:47 +0100

    TIC TAC TOE

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const cellElements = document.querySelectorAll("[data-cell]")
 const screenMessageTextElement = document.querySelector("[data-screen-message-text]")
 const screenMessageElement = document.getElementById("screenMessage")
 const resetButton = document.getElementById("resetButton")
+const restartButton = document.getElementById("restartButton")
 let circleTurn
 
+// condition de victoire 5 lignes
 const winning = [
     [0,1,2,3,4],
     [5,6,7,8,9],
@@ -22,10 +24,17 @@ const winning = [
     [4,8,12,16,20]
 ]
 
+// appel de fonction reset
 startGame()
 
+// 2 boutons reset qui lance ma fonction start game
 resetButton.addEventListener("click", startGame)
+restartButton.addEventListener("click", startGame)
 
+
+// descirption le tour n'est pas le tour cercle, 
+// pour chaque class des cellules j'enleve le "x" ou "circle" / je retire la condition d'un seul click par cellule et je la remets pour reset
+// j'enleve ma page de fin de jeu
 function startGame(){
     circleTurn = false
     cellElements.forEach(cell => {
@@ -36,6 +45,13 @@ function startGame(){
 })
 screenMessageElement.classList.remove("active")
 }
+
+
+// FONCTION à chaque clic sur cellule
+// la cellule devient la cible
+// est-ce que c'est au tour des cercles ? si oui currentClass= Circle_class sinon ça devient X_class
+// appel de ma fonction placemark (ajout de "x" ou "circle" dans la class div cell qui modifie l'apparence)
+// verif de la condition de victoire renvoie à l'ecran de fin sinon si c'est un match nul renvoie à l'ecran de fin sinon change de tour
 
 function handleclick(e){
     const cell = e.target
@@ -50,11 +66,15 @@ function handleclick(e){
     }
 }
 
+// fonction match nul vérifie si sur chaque cellule il y a X_class ou Circle_class
+
 function isDraw() {
     return [...cellElements].every(cell => {
         return cell.classList.contains(X_class) || cell.classList.contains(Circle_class)
     })
 }
+
+//fonction fin de game si c'est un match nul, affiche le 2eme ecran avec écrit "match nul" sinon affiche dans le texte le nom du joueur a gagné
 
 function endGame(draw) {
     if(draw){
@@ -65,13 +85,19 @@ function endGame(draw) {
     screenMessageElement.classList.add("active")
 }
 
+//fonction changement de tour, circleTurn passe de true a false ou inversement
+
 function swapTurns(){
     circleTurn = !circleTurn
 }
 
+
+//fonction de modification de la class cell en lui donnant soit X_class("div class=cell x") ou Circle_class ("div class=cell circle")
 function placeMark(cell, currentClass){
     cell.classList.add(currentClass)
 }
+
+// fonction de win pour chaque combinaison vérifie que les combinaisons gagnantes fassent partie des combinaisons trouvables dans le [] de currentClass
 
 function checkWin(currentClass){
     return winning.some(combination => {
